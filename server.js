0edd668c787cdbd34dff16a13d22c133ff139035
Prakhar Srivastava <prakharsrvstv14@gmail.com>
2024-01-12 00:27:14 +0530

    cors changed back

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({
   credentials: true,
-  origin: (origin, callback) => {
-    callback(null, true);
-  }
+  // origin: (origin, callback) => {
+  //   callback(null, true);
+  // }
 }));
 
 
@@ -40,7 +40,7 @@ main()
 
 //headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
   next();
