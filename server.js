40b4fa8b470839f659bf637f51fafed13987421c
Prakhar Srivastava <prakharsrvstv14@gmail.com>
2024-01-12 22:54:43 +0530

    sj

--- server.js
+++ server.js
@@ -37,18 +37,18 @@ async function main() {
 main();
 
 //headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, PUT"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, PUT"
+//   );
+//   next();
+// });
 //ROUTES
 app.use("/api", getTeamRoute);
 app.use("/api", eventRegistrationRoute);
