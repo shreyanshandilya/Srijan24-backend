575086b3c6733f6cf139951dc05e0229c423c45b
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2024-01-14 15:48:24 +0530

    add

--- controllers/checkOutPaymnetControllers.js
+++ controllers/checkOutPaymnetControllers.js
@@ -8,6 +8,7 @@ const MakeOrder = async (req, res, next) => {
     const razorpay =  new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_SECRET,
+      api_version: '1.0',
     });
     const options = req.body;
     console.log(options);
