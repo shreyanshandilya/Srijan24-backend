6b894780d9aa1fdb648494bf77660376ac4b467e
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2024-01-14 13:55:18 +0530

    add

--- controllers/checkOutPaymnetControllers.js
+++ controllers/checkOutPaymnetControllers.js
@@ -1,71 +1,94 @@
-const HttpError = require('../utils/HttpError');
-const Razorpay =require('razorpay');
-const crypto =require('crypto');
+const HttpError = require("../utils/HttpError");
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 const User = require("../schemas/userSchema");
 
-const MakeOrder=  async (req, res ,next) => {
-    try {
-      const razorpay = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY_ID,
-        key_secret: process.env.RAZORPAY_SECRET,
-      });
-      const options = req.body;
-      const order = await razorpay.orders.create(options);
-      if (!order) {
-        return ;
-      }
-      res.json(order);
-    } catch (err) {
-      console.log(err);
-      return next(new HttpError('Error' ,500));
+const MakeOrder = async (req, res, next) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+    const options = req.body;
+    console.log(options);
+    const order = await razorpay.orders.create(options);
+    if (!order) {
+      return;
     }
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Error occured", 500));
+  }
+};
+
+const ValidateOrderPayment = async (req, res, next) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  console.log(req.body);
+  const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+  const digest = sha.digest("hex");
+
+  if (digest !== razorpay_signature) {
+    return next(
+      new HttpError(
+        "Transaction failed , money will be refunded in 4-5 days if debited",
+        400
+      )
+    );
   }
 
- const ValidateOrderPayment =  async (req, res ,next) => {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
-  
-    const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
-    sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
-    const digest = sha.digest("hex");
+  const userId = req.userData.UserId;
+  console.log(userId);
+  let response;
+  try {
+    response = await User.findById(userId);
+  } catch (error) {
+    return next(
+      new HttpError(
+        "Transaction failed , money will be refunded in 4-5 days if debited",
+        400
+      )
+    );
+  }
+  console.log(response);
 
-    if (digest !== razorpay_signature) {
-      return next(new HttpError('Transaction failed , money will be refunded in 4-5 days if debited' ,400));
-    }
-     
-    const userID = req.userData.userID;
-    let response ;
-    try{
-      response = await User.findById(userID);
-    }catch(error){
-      return next(new HttpError('Transaction failed , money will be refunded in 4-5 days if debited' ,400));
-    }
+  let orderID = razorpay_order_id;
+  let paymentID = razorpay_payment_id;
+  let tshirtSize = req.body.tshirtSize;
+  let quantity = req.body.quantity;
+  let address = req.body.addresss;
 
-    let  orderID= razorpay_order_id;
-    let paymentID = razorpay_payment_id;
-    let tshirtSize = req.body.tshirtSize;
-    let quantity = req.body.quantity;
-    let address = req.body.address;
+  response.Merchandise.push({
+    tshirtSize: tshirtSize,
+    address: address,
+    quantity: quantity,
+    orderID: orderID,
+    paymentID: paymentID,
+  });
+  let userr;
 
-    response.Merchandise.push({
-      tshirtSize: tshirtSize,
-      address: address,
-      quantity: quantity,
-      orderID :orderID,
-      paymentID :paymentID
-    });
-    
-    try{
-      await response.save();
-    }catch{
-      return next(new HttpError('Transaction failed , money will be refunded in 4-5 days if debited' ,400));
-    }
+  console.log(response);
+  try {
+    userr = await response.save();
+  } catch {
+    return next(
+      new HttpError(
+        "Transaction failed ",
+        400
+      )
+    );
+  }
+  console.log(userr);
 
-    res.json({
-      msg: "success",
-      orderId: razorpay_order_id,
-      paymentId: razorpay_payment_id,
-    });
-  };
+  res.json({
+    msg: "success",
+    orderId: razorpay_order_id,
+    paymentId: razorpay_payment_id,
+  });
+};
 
-  exports.MakeOrder=MakeOrder;
-  exports.ValidateOrderPayment=ValidateOrderPayment;
+exports.MakeOrder = MakeOrder;
+exports.ValidateOrderPayment = ValidateOrderPayment;
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -47,7 +47,7 @@ const userSchema = new mongoose.Schema({
         required:true
       },
       quantity: {
-        type: Number,
+        type: String,
         required: true,
       },
     },
