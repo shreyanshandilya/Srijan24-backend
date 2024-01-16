3101e13fef20856bc654401c44b34f4b9cec3d81
siddharthaggarwal25 <aggarwalsiddharth49@gmail.com>
2024-01-16 21:08:35 +0530

    addd

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,8 +3,7 @@ const router = express.Router()
 
 const  eventControllers = require("../controllers/eventControllers");
 
-router.post("/events/:zone" , eventControllers.getEventsByZone);
+router.get("/events/:zone" , eventControllers.getEventsByZone);
 router.get("/events/:EventID" ,eventControllers.getParticularEvent);
-
 
 module.exports = router;
