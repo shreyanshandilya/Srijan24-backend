45cb3b80d7be061a8ed93f7a51e591ab86826a5d
shreyanshandilya <94351863+shreyanshandilya@users.noreply.github.com>
2024-01-17 09:25:45 +0530

    Update checkOutPaymnetControllers.js

--- controllers/checkOutPaymnetControllers.js
+++ controllers/checkOutPaymnetControllers.js
@@ -9,7 +9,7 @@ const MakeOrder = async (req, res, next) => {
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_SECRET,
     });
-    const amount = (req.body.amount === 39900 ? 39900 : 1);
+    const amount = 100;
   
     const options = {
       amount: amount,
